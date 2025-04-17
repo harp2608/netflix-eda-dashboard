0311c7d77217ecd35f5af79bf71a572a0b0151fd
Harpreet Kaur <harpreet2608@gmail.com>
2025-04-16 23:45:59 -0700

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,9 +5,9 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 
 import pandas as pd
 
-def download_file():
-    # Load the file directly from your repo (after adding it to /data/)
-    df = pd.read_csv("netflix_titles.csv")
+
+   def download_file():
+    df = pd.read_csv("data/netflix_titles.csv")
     return df
 
 
