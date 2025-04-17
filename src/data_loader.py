71c03cde7b6d1e65ec918e81f7992cdfb55327f8
Harpreet Kaur <harpreet2608@gmail.com>
2025-04-16 23:58:42 -0700

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,8 +5,7 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 
 import pandas as pd
 
-
-   def download_file():
+def download_file():
     df = pd.read_csv("data/netflix_titles.csv")
     return df
 
